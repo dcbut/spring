9855b380daf0787e3e2c7b21f9121785b8c5d4cd
Dchuang <dc.dongchuang@qq.com>
2022-10-29 22:52:37 +0800

    Spring项目AOP百度网盘密码数据兼容处理

---
+++ spring_14_aop_case_handle_password/src/main/java/com/dcxuexi/dao/ResourcesDao.java
@@ -0,0 +1,12 @@
+package com.dcxuexi.dao;
+
+/***
+ * @Title ResourcesDao
+ * @Description TOTD
+ * @Auter DongChuang
+ * @Date 2022/10/29 21:47
+ * @Version 1.0.0
+ */
+public interface ResourcesDao {
+    boolean readResources(String url, String password);
+}
